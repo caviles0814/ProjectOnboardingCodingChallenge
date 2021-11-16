d561f99188cbfad11f09793432769842b7645428
caviles0814 <61719581+caviles0814@users.noreply.github.com>
2021-11-16 18:42:18 -0400

    Create redundant.js

---
+++ redundant.js
@@ -0,0 +1,22 @@
+
+
+function redundantMain(strMain){
+
+
+return redundant (strMain)
+    
+}
+
+function redundant(strng){
+
+
+
+
+    return strng;
+}
+const f1 =redundantMain("Banana");
+const f2 =redundantMain("");
+const f3 =redundantMain("Hello World");
+console.log(f1 );
+console.log(f2 );
+console.log(f3 );
